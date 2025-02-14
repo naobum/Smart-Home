e6d5f2be22a89ec4343d717e8271e0774bc5ba10
Student <Student@NCH-02-406-14>
2025-02-14 14:38:28 +0300

    Добавьте файлы проекта.

---
+++ Smart Home/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
